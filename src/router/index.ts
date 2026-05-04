2a8201dcada83397d8f10c8463dfc982b34c7715
Jack An <3098461553@qq.com>
2026-05-04 12:06:06 +0800

    chore: 配置 GitHub Pages 自动部署

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import BlogLayout from '../layout/BlogLayout.vue'
 import Home from '../views/Home.vue'
 import Post from '../views/Post.vue'
 import About from '../views/About.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
